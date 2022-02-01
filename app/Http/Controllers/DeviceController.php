4649c5084d69a51f2d735c3c440f7183684c7870
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2022-02-01 18:00:33 +0545

    api post data

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Device;
 use Illuminate\Http\Request;
 
 class DeviceController extends Controller
 {
-    //
+    // public function index()
+    // {
+    //     return Device::all();
+    // }
+    public function showData($id=null)
+    {
+        return  $id==null ? Device::all() : Device::find($id) ;
+    }
+    public function postData(Request $request){
+        $device = new Device;
+        $device->name = $request->name;
+        $device->save();
+        return ['status'=>'success'];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DeviceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+// Route::get('/device', [DeviceController::class, 'index']);
+Route::get('/device/{id?}', [DeviceController::class, 'showData']);
+//  ? means optional
+Route::post('/post', [DeviceController::class, 'postData']);
+
